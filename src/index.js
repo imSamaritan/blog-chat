c1c7f199844750de0abc36269ab2645ce66b8b7c
imsamaritan <aicodemate.talk@gmail.com>
2022-12-01 21:22:34 +0200

    Created a Header component, it template from templates dir, rendered on html page and added style for the row class + imported banner image

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import './sass/main.scss';
 import 'bootstrap';
+import banner from './assets/banner.jpg';
+import Header from './components/views/Header';
+
+customElements.define('app-header', Header);
