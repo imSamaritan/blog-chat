a4045f9837e5c1f0df5b5d4559abcf53746f072e
iamSamaritan <aicodemate.talk@gmail.com>
2022-12-04 19:46:15 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 import './sass/main.scss';
 import 'bootstrap';
 import banner from './assets/banner.jpg';
-import Header from './components/views/Header';
-import Card from './components/shared/Card';
-
-customElements.define('app-header', Header);
-customElements.define('app-card', Card);
