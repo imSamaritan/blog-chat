e92f69e03194baf65e8c2c3e53f4e18912f07be1
imsamaritan <aicodemate.talk@gmail.com>
2022-12-05 18:58:58 +0200

    Created a card, rendered it to the html page

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import 'bootstrap';
 import banner from './assets/banner-cropped.jpg';
 import App from './components/app/App';
 import Header from './components/views/Header';
+import Card from './components/shared/Card';
